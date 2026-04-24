5a748b4594c9decdb0404cd7c4a6e33cf0a19e0c
zhongyang-yu <97617749+zhongyang-yu@users.noreply.github.com>
2026-04-24 08:30:34 +0000

    Deploying to gh-pages from @ zhongyang-yu/zhongyang-yu.github.io@78398475813d5e5ea66f2577289e95bcac56873c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/blog/";
+            window.location.href = "https://lyzy.github.io/index.html";
           },
         },{id: "nav-publications",
           title: "publications",
