f14fb76709895ae7553ed52e5ae42954385413cd
zhongyang-yu <97617749+zhongyang-yu@users.noreply.github.com>
2026-09-10 13:43:35 +0000

    Deploying to gh-pages from @ zhongyang-yu/zhongyang-yu.github.io@4d66a17b8235cef6799e27c68ae8506206e999a8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Curriculum Vitae of Zhongyang YU. You can also download the PDF version from the top-right button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
